5a2912b2207242c38671f74ddbca23d4c2b5dc28
Adam Sandham <z_a_sandham@hotmail.co.uk>
2024-06-15 00:38:11 +0100

    added sales data to sales worksheet

--- run.py
+++ run.py
@@ -16,15 +16,20 @@ def get_sales_date():
     """
         Get sales figures input from the user
     """
+    while True:
+        print("please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
+        
+        data_str = input("Enter your data here: ")
     
-    print("please enter sales data from the last market.")
-    print("Data should be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
     
-    data_str = input("Enter your data here: ")
-   
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+    return sales_data
     
 def validate_data(values):
    
@@ -34,13 +39,26 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
-        if len(value) != 6:
+        [int(value) for value in values]
+        if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-        
+        return False
     
+    return True
     
-get_sales_date()
+data = get_sales_date()
+sales_date = [int(num) for num in data]
+
+def update_sales_worksheet():
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+
+    print("Updating sales worksheet ....\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet = SHEET.append_row(data)
+    print("Sales worksheet updated successfully.\n")
