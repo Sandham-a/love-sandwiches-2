3ab5d8272c956d80155d9a7185407758443704ec
sandham-a <z_a_sandham@hotmail.co.uk>
2024-06-14 11:55:51 +0100

    Create get_sales_data function to get data string from the user

--- run.py
+++ run.py
@@ -22,3 +22,6 @@ def get_sales_date():
     print("Example: 10,20,30,40,50,60")
     
     data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
+
+get_sales_date()
